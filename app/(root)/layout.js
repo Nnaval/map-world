0e6ea37c6ec672cc9d49c96a6771eead610c6092
Emmanuel-xt <fattmane5@gmail.com>
2024-12-30 14:59:16 +0100

    kick off

---
+++ app/(root)/layout.js
@@ -0,0 +1,29 @@
+import { Inter } from "next/font/google";
+import "@styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "FUTO-VERSE",
+  description: "Shaping the future of Campus Navigation and Business Promotion",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {/* <Provider>
+          <LocationProvider>
+            <CesiumViewerProvider> */}
+        <main className="">
+          <div className="">{children}</div>
+        </main>
+        {/* <Bottombar />
+              <CesiumMapB />
+            </CesiumViewerProvider>
+          </LocationProvider>
+        </Provider> */}
+      </body>
+    </html>
+  );
+}
