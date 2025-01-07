567e1b98cfc97543d68cf3129294ea4b3f8d03f5
Ruzz <nnaemeka.obiefuna@gmail.com>
2025-01-07 20:42:55 +0100

    auth pages updated

---
+++ app/(auth)/sign-up/page.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import { AiOutlineEye } from "react-icons/ai";
+
+const page = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
+      <h1 className="text-3xl font-extrabold text-gray-800 mb-4 text-center">Create an account</h1>
+      <p className='font-semibold text-center'>Please fill your details</p>
+      <form>
+       
+        <div className="mb-4">
+          <label htmlFor="fullName" className="block text-gray-700 font-medium mb-2">
+            Full Name
+          </label>
+          <input
+            type="text"
+            id="fullName"
+            name='fullName'
+            className="w-full px-4 py-2 border cursor-pointer rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Enter your full name"
+          />
+        </div>
+
+        
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-gray-700 font-medium mb-2">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            name='username'
+            className="w-full px-4 py-2 border cursor-pointer rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Enter your username"
+          />
+        </div>
+
+       
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700 font-medium mb-2">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name='email'
+            className="w-full px-4 py-2 border cursor-pointer rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Enter your email"
+          />
+        </div>
+
+        
+        <div className="mb-4 relative">
+          <label htmlFor="password" className="block text-gray-700 font-medium mb-2">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name='password'
+            className="w-full px-4 py-2 border cursor-pointer rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Enter your password"
+          />
+          <span className="absolute inset-y-0 right-4 flex items-center text-gray-500">
+          <AiOutlineEye  className='font-extrabold text-2xl mt-6'/>
+          </span>
+        </div>
+
+        
+        <div className="mb-4 relative">
+          <label htmlFor="confirmPassword" className="block text-gray-700 font-medium mb-2">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name='confirmPassword'
+            className="w-full px-4 py-2 border cursor-pointer rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Confirm your password"
+          />
+          <span className="absolute inset-y-0 right-4 flex items-center text-gray-500">
+          <AiOutlineEye  className='font-extrabold text-2xl mt-6'/>
+          </span>
+        </div>
+
+        
+        <button
+          type="submit"
+          className="w-full bg-blue-700 text-white py-2 px-4 mt-6 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          Sign-Up
+        </button>
+      </form>
+    </div>
+  </div>
+  )
+}
+
+export default page
