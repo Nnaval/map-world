69d25f077e7588118ed453491a0f5c25dc67de16
Ruzz <nnaemeka.obiefuna@gmail.com>
2025-03-19 03:00:54 +0100

    comm

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -2,7 +2,7 @@ import ButtomNav from "@components/ButtomNav";
 import React from "react";
 
 const page = () => {
-  return <div className="">Home Pag here</div>;
+  return <div className=""> Page</div>;
 };
 
 export default page;
