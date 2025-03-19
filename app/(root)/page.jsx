9a3548826b9eb8b9c02db120852de1954d4008c2
Ruzz <nnaemeka.obiefuna@gmail.com>
2025-03-19 02:39:57 +0100

    few text added in the home page

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -2,7 +2,7 @@ import ButtomNav from "@components/ButtomNav";
 import React from "react";
 
 const page = () => {
-  return <div className="">Page</div>;
+  return <div className="">Home Page here</div>;
 };
 
 export default page;
