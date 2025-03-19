4b0bf105a65a9a4c0b03bb9eacdeadbe188befed
Ruzz <nnaemeka.obiefuna@gmail.com>
2025-03-19 02:51:28 +0100

    home text

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -2,7 +2,7 @@ import ButtomNav from "@components/ButtomNav";
 import React from "react";
 
 const page = () => {
-  return <div className="">Home Page here</div>;
+  return <div className="">Home Pag here</div>;
 };
 
 export default page;
